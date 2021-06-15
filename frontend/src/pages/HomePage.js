02723da811fcd951bb14209162da5f28337db9d9
farid <farid@haiderzada.de>
2021-06-15 10:34:59 +0200

    Fixed pom.xml

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,3 +1,40 @@
+import styled from "styled-components/macro";
+import {Button, Text, TextInput} from "@primer/components";
+
 export default function HomePage() {
-    return null;
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+    }
+
+   return (
+       <Wrapper>
+        <form onSubmit={handleSubmit}>
+            <Wrapper>
+                <Text>
+                    Incredients
+                    <TextInput
+                        type="text"
+                        name="incredients"
+                        value="incredients"
+                    />
+                </Text>
+                <Button>Search</Button>
+            </Wrapper>
+        </form>
+    </Wrapper>
+)
 }
+
+const Wrapper = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+        
+    *:not(img):not(a){
+    margin: 10px;
+    }
+`
