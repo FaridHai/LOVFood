5487fcc71b71a3992adf36973a6f0dfca0ccc483
Faridhai <ha.farid@ymail.com>
2021-07-06 11:10:56 +0200

    Backend Tests Done!

--- backend/src/test/java/de/haiderzada/backend/service/IngredientsServiceTest.java
+++ backend/src/test/java/de/haiderzada/backend/service/IngredientsServiceTest.java
@@ -1,2 +1,34 @@
-package de.haiderzada.backend.service;public class IngredientsServiceTest {
+package de.haiderzada.backend.service;
+
+
+import de.haiderzada.backend.model.Ingredient;
+import de.haiderzada.backend.repository.IngredientsRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.Mockito.*;
+
+public class IngredientsServiceTest {
+
+    private final IngredientsRepository repository = mock(IngredientsRepository.class);
+    private final IngredientsService service = new IngredientsService(repository);
+
+    @Test
+    public void listIngredientsShouldReturnIngredientsFromRepository() {
+        //GIVEN
+        when(repository.findAll()).thenReturn(List.of(
+                new Ingredient("4002", "Manti"),
+                new Ingredient("3003", "Teigtasche")));
+
+        //WHEN
+        List<Ingredient> ingredientList = service.listIngredients();
+
+        //THEN
+        assertThat(ingredientList, containsInAnyOrder(
+                new Ingredient("4002", "Manti"),
+                new Ingredient("3003", "Teigtasche")));
+    }
 }
